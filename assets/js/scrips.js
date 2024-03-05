e05b901357ba3f5c0e71eef1841998a3d1ebcd49
andrea <arodriguezlopez@gmail.com>
2024-03-05 10:26:43 -0300

    agregue boton de enviar formulario con tooltips

---
+++ assets/js/scrips.js
@@ -0,0 +1,6 @@
+// habilitacion tooltips
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+// habilitacion popovers
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
