742e76396ba555cbfce3858cf90bd918ff703daf
andrea <arodriguezlopez@gmail.com>
2024-03-13 02:26:06 -0300

    se agrega alerta de enviar mensaje

--- assets/js/scrips.js
+++ assets/js/scrips.js
@@ -4,3 +4,7 @@ const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstra
 // habilitacion popovers
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+$("#enviar_correo").on("click", function () {
+    alert("El correo fue enviado correctamente.");
+});
